084c049a2eb3a1395f8e350346af1c6b9f6ae369
Fatima Salman <fa_salman@hotmail.com>
2026-07-23 21:18:43 +0000

    connect frontend to vercel api

--- artifacts/gaza-future-academy/src/main.tsx
+++ artifacts/gaza-future-academy/src/main.tsx
@@ -1,4 +1,4 @@
-import { setBaseUrl } from "../../lib/api-client-react";
+import { setBaseUrl } from "@workspace/api-client-react";
 setBaseUrl('https://gaza-future-academy-api-server-8juedfqei.vercel.app');
 import { createRoot } from 'react-dom/client';
 
